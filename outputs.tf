e2316f48fe73c7014ea21078882c4fb06f1c8039
Gabriel <gsilva.gabriel@outlook.com.br>
2024-12-01 17:22:25 -0300

    atualização

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "table_arn" {
-  value = aws_dynamodb_table.preferencias.arn
-}
-
 output "table_name" {
   value = aws_dynamodb_table.preferencias.name
+}
+
+output "table_arn" {
+  value = aws_dynamodb_table.preferencias.arn
 }
