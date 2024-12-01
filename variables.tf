326b9db21346c939eb01c1e0c5232dbb54c5aa96
Gabriel <gsilva.gabriel@outlook.com.br>
2024-12-01 16:45:12 -0300

    atualização

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "A região da AWS onde os recursos serão criados"
   type        = string
-  default     = "sa-east-1"
+  default     = "us-east-1"
 }
 
 variable "dynamodb_table_name" {
